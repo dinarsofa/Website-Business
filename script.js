5ed5949521eb2a7fb7a6a9afb2980b2c4f6170e5
dinarsofa <140148991+dinarsofa@users.noreply.github.com>
2023-07-21 15:00:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+  if (window.confirm("Apakah Anda sudah berusia di atas 18 tahun?")) {
+    
+  } else {
+    window.location.href = "https://www.soryy";
+  }
+}, 5000); // 5000 milliseconds = 5 seconds
